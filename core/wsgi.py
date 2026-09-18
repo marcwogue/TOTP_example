a390427e995b73fa91bf67f97eb5f63a88a8a184
marc <marc.wogue@gmail.com>
2026-09-18 11:49:30 +0100

    totp demo application

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
+import sys 
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
 
